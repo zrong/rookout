f0ddb3d57da12755b2c918e3b29806a202eeba77
zrong <zrongzrong@gmail.com>
2014-10-09 15:19:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zrong",
+    version = "0.1",
+    description = "A python library by zrong",
+    author = "zrong",
+    author_email = "zrongzrong@gmail.com",
+    url = "http://github.com/zrong/python",
+    license = "MIT",
+    keywords = "utils development 1201",
+    packages = find_packages(exclude=["tests*","docs"]),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
