394ebaf508099072d8fba4d8d1c3212f3b3d6e0a
zrong <zrongzrong@gmail.com>
2015-04-30 10:32:08 +0800

    add ColoredStreamHandler

--- zrong/__init__.py
+++ zrong/__init__.py
@@ -6,7 +6,7 @@
 
 import logging
 
-__version__ = "0.1.5"
+__version__ = "0.3.1"
 __all__ = ['base', 'git', 'lua', 'ftp', 'gettext']
 
 slog = logging.getLogger("system")
@@ -18,6 +18,54 @@ class ZrongError(Exception):
     """zrong 模块使用的异常类。"""
     pass
 
+
+# 可改为使用：https://github.com/limodou/uliweb/blob/master/uliweb/utils/coloredlog.py
+# 和 https://github.com/tartley/colorama
+class ConsoleColor():
+	RESET = "\033[0m"
+	BOLD = "\033[1m"
+	UNDERLINE = "\033[4m"
+	BLINK = "\033[5m"
+	REVERSE = "\033[7m"
+	FADE = "\033[8m"
+
+	BLACK = "\033[30m"
+	RED = "\033[31m"
+	GREEN = "\033[32m"
+	YELLOW = "\033[33m"
+	BLUE = "\033[34m"
+	MAGENTA = "\033[35m"
+	CYAN = "\033[36m"
+	WHITE = "\033[37m"
+
+	BG_BLACK = "\033[40m"
+	BG_RED = "\033[41m"
+	BG_GREEN = "\033[42m"
+	BG_YELLOW = "\033[43m"
+	BG_BLUE = "\033[44m"
+	BG_MAGENTA = "\033[45m"
+	BG_CYAN = "\033[46m"
+	BG_WHITE = "\033[47m"
+
+        # 黑底彩色
+	# B2_BLACK = "\033[90m"
+	# B2_RED = "\033[91m"
+	# B2_GREEN = "\033[92m"
+	# B2_YELLOW = "\033[93m"
+	# B2_BLUE = "\033[94m"
+	# B2_MAGENTA = "\033[95m"
+	# B2_CYAN = "\033[96m"
+	# B2_WHITE = "\033[97m"
+
+
+class ColoredStreamHandler(logging.StreamHandler):
+    def format(self, record):
+        msg = super(ColoredStreamHandler, self).format(record)
+        if record.levelno >= logging.ERROR:
+            msg = ConsoleColor.RED+msg+ConsoleColor.RESET
+        elif record.levelno == logging.WARNING:
+            msg = ConsoleColor.YELLOW+msg+ConsoleColor.RESET
+        return msg
 
 def add_log_handler(log, handler=None, debug=None, fmt=None):
     """为一个 :class:`logging.Logger` 的实例增加 handler。
